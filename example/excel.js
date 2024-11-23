3f1dbfeb89e841b0a6042fc77e8e596df80880f8
Luigi Tavolato <luigi.tavolato@qberg.com>
2024-11-23 22:54:52 +0100

    Samples

---
+++ example/excel.js
@@ -0,0 +1,53 @@
+// https://github.com/vinubabu323/strapi-plugin-excel-export
+module.exports = {
+    config: {
+        "api::product.product": {
+            columns: [
+                "name",
+                "brand",
+                "description",
+                "code",
+                "location",
+                "weight",
+                "size"
+            ],
+            relations: {
+                prices: {
+                    columns: ["unit_cost"],
+                    relations: {
+                        supplier: {
+                            columns: ["name"]
+                        }
+                    }
+                },
+                category: {
+                    columns: ["name"],
+                },
+                type: {
+                    columns: ["name"],
+                },
+                events: {
+                    columns: ["name"],
+                },
+                certifications: {
+                    columns: ["name"],
+                },
+            },
+            locale: "false",
+            labels: {
+                "name": "Nome",
+                "brand": "Marca",
+                "description": "Descrizione",
+                "code": "Riferimento Interno",
+                "weight": "Peso",
+                "size": "Dimensione",
+                "location": "Posizione",
+                "category": "Categoria Prodotto",
+                "type": "Tipo Prodotto",
+                "events": "Eventi",
+                "certifications": "Certificazioni",
+                "prices": "Prezzo di vendita"
+            }
+        }
+    }
+};
